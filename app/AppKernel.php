3be825fd953e232fb619cc0c846d193aa1080bcf
Xavier Leune <xleune@commentcamarche.net>
2015-01-19 09:46:50 +0100

    Externalisation du ccmbenchmark/documentation-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new CCMBenchmark\DocumentationBundle\CCMBenchmarkDocumentationBundle(),
+            new \AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/CCMBenchmark/DocumentationBundle/CCMBenchmarkDocumentationBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CCMBenchmark\DocumentationBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CCMBenchmarkDocumentationBundle extends Bundle
-{
-}
